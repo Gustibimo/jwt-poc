c67bda301457ae30d10e4e6ebb81dcdc70121c2b
Gustibimo <gstbimo@gmail.com>
2019-02-01 21:17:39 +0700

    Add functionality to routes

--- main.go
+++ main.go
@@ -15,10 +15,9 @@ func main() {
 	// /status - which we will call to make sure that our API is up and running
 	// /products - which will retrieve a list of products that the user can leave feedback on
 	// /products/{slug}/feedback - which will capture user feedback on products
-	r.Handle("/status", NotImplemented).Methods("GET")
-	r.Handle("/products", NotImplemented).Methods("GET")
-	r.Handle("/products/{slug}/feedback", NotImplemented).Methods("POST")
-
+	r.Handle("/status", StatusHandler).Methods("GET")
+  r.Handle("/products", ProductsHandler).Methods("GET")
+  r.Handle("/products/{slug}/feedback", AddFeedbackHandler).Methods("POST")
 	// We will setup our server so we can serve static assest like images, css from the /static/{file} route
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
