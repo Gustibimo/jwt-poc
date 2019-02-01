c5248a1ed7742542cd364ed8fe287e8db31eee9e
Gustibimo <gstbimo@gmail.com>
2019-02-01 21:12:08 +0700

    Add some handler

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// Here we are instantiating the gorilla/mux router
+	r := mux.NewRouter()
+	// On the default page we will simply serve our static index page.
+	r.Handle("/", http.FileServer(http.Dir("./views/")))
+	// Our API is going to consist of three routes
+	// /status - which we will call to make sure that our API is up and running
+	// /products - which will retrieve a list of products that the user can leave feedback on
+	// /products/{slug}/feedback - which will capture user feedback on products
+	r.Handle("/status", NotImplemented).Methods("GET")
+	r.Handle("/products", NotImplemented).Methods("GET")
+	r.Handle("/products/{slug}/feedback", NotImplemented).Methods("POST")
+
+	// We will setup our server so we can serve static assest like images, css from the /static/{file} route
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+
+	// Our application will run on port 3000. Here we declare the port and pass in our router.
+	http.ListenAndServe(":3000", r)
+}
+
+// Here we are implementing the NotImplemented handler. Whenever an API endpoint is hit
+// we will simply return the message "Not Implemented"
+var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Not Implemented"))
+})
