89e2b494b80b34ddaa8bc74e1319ce9db2cf261a
Gustibimo <gstbimo@gmail.com>
2019-02-02 09:52:42 +0700

    refactor middleware function

--- main.go
+++ main.go
@@ -2,126 +2,141 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"strings"
 	"time"
 
-	"github.com/gorilla/mux"
-	jwtmiddleware "github.com/nicovillanueva/go-jwt-middleware"
-	jwt "github.com/vladimiroff/jwt-go"
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/rs/cors"
 )
 
+const (
+	PORT   = "1337"
+	SECRET = "lazyme"
+)
+
+type JWTData struct {
+	// Standard claims are the standard jwt claims from the IETF standard
+	// https://tools.ietf.org/html/rfc7519
+	jwt.StandardClaims
+	CustomClaims map[string]string `json:"custom,omitempty"`
+}
+
+type Account struct {
+	Email    string  `json:"email"`
+	Balance  float64 `json:"balance"`
+	Currency string  `json:"currency"`
+}
+
 func main() {
-	// Here we are instantiating the gorilla/mux router
-	r := mux.NewRouter()
-	// On the default page we will simply serve our static index page.
-	r.Handle("/", http.FileServer(http.Dir("./views/")))
-	// Our API is going to consist of three routes
-	// /status - which we will call to make sure that our API is up and running
-	// /products - which will retrieve a list of products that the user can leave feedback on
-	// /products/{slug}/feedback - which will capture user feedback on products
-	r.Handle("/status", NotImplemented).Methods("GET")
-	/* We will add the middleware to our products and feedback routes. The status route will be publicly accessible */
-	r.Handle("/products", jwtMiddleware.Handler(ProductsHandler)).Methods("GET")
-	r.Handle("/products/{slug}/feedback", jwtMiddleware.Handler(AddFeedbackHandler)).Methods("POST")
-	r.Handle("/get-token", GetTokenHandler).Methods("GET")
-	// We will setup our server so we can serve static assest like images, css from the /static/{file} route
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/account", account)
 
-	// Our application will run on port 3000. Here we declare the port and pass in our router.
-	http.ListenAndServe(":3000", r)
+	handler := cors.Default().Handler(mux)
+
+	log.Println("Listening for connections on port: ", PORT)
+	log.Fatal(http.ListenAndServe(":"+PORT, handler))
 }
 
-// Here we are implementing the NotImplemented handler. Whenever an API endpoint is hit
-// we will simply return the message "Not Implemented"
-var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Not Implemented"))
-})
-
-/* We will first create a new type called Product
-   This type will contain information about VR experiences */
-type Product struct {
-	Id          int
-	Name        string
-	Slug        string
-	Description string
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from Go!")
 }
 
-/* We will create our catalog of VR experiences and store them in a slice. */
-var products = []Product{
-	Product{Id: 1, Name: "Hover Shooters", Slug: "hover-shooters", Description: "Shoot your way to the top on 14 different hoverboards"},
-	Product{Id: 2, Name: "Ocean Explorer", Slug: "ocean-explorer", Description: "Explore the depths of the sea in this one of a kind underwater experience"},
-	Product{Id: 3, Name: "Dinosaur Park", Slug: "dinosaur-park", Description: "Go back 65 million years in the past and ride a T-Rex"},
-	Product{Id: 4, Name: "Cars VR", Slug: "cars-vr", Description: "Get behind the wheel of the fastest cars in the world."},
-	Product{Id: 5, Name: "Robin Hood", Slug: "robin-hood", Description: "Pick up the bow and arrow and master the art of archery"},
-	Product{Id: 6, Name: "Real World VR", Slug: "real-world-vr", Description: "Explore the seven wonders of the world in VR"},
-}
+func login(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Login failed!", http.StatusUnauthorized)
+	}
 
-/* The status handler will be invoked when the user calls the /status route
-   It will simply return a string with the message "API is up and running" */
-var StatusHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("API is up and running"))
-})
+	var userData map[string]string
+	json.Unmarshal(body, &userData)
 
-/* The products handler will be called when the user makes a GET request to the /products endpoint.
-   This handler will return a list of products available for users to review */
-var ProductsHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	// Here we are converting the slice of products to JSON
-	payload, _ := json.Marshal(products)
+	// Demo - in real case scenario you'd check this against your database
+	if userData["email"] == "admin@gmail.com" && userData["password"] == "admin123" {
+		claims := JWTData{
+			StandardClaims: jwt.StandardClaims{
+				ExpiresAt: time.Now().Add(time.Hour).Unix(),
+			},
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(payload))
-})
-
-/* The feedback handler will add either positive or negative feedback to the product
-   We would normally save this data to the database - but for this demo, we'll fake it
-   so that as long as the request is successful and we can match a product to our catalog of products
-   we'll return an OK status. */
-var AddFeedbackHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	var product Product
-	vars := mux.Vars(r)
-	slug := vars["slug"]
-
-	for _, p := range products {
-		if p.Slug == slug {
-			product = p
+			CustomClaims: map[string]string{
+				"userid": "u1",
+			},
 		}
-	}
 
-	w.Header().Set("Content-Type", "application/json")
-	if product.Slug != "" {
-		payload, _ := json.Marshal(product)
-		w.Write([]byte(payload))
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		tokenString, err := token.SignedString([]byte(SECRET))
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Login failed!", http.StatusUnauthorized)
+		}
+
+		json, err := json.Marshal(struct {
+			Token string `json:"token"`
+		}{
+			tokenString,
+		})
+
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Login failed!", http.StatusUnauthorized)
+		}
+
+		w.Write(json)
 	} else {
-		w.Write([]byte("Product Not Found"))
+		http.Error(w, "Login failed!", http.StatusUnauthorized)
 	}
-})
+}
 
-/* Set up a global string for our secret */
-var mySigningKey = []byte("secret")
+func account(w http.ResponseWriter, r *http.Request) {
+	authToken := r.Header.Get("Authorization")
+	authArr := strings.Split(authToken, " ")
 
-/* Handlers */
-var GetTokenHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	/* Create the token */
-	token := jwt.New(jwt.SigningMethodHS256)
+	if len(authArr) != 2 {
+		log.Println("Authentication header is invalid: " + authToken)
+		http.Error(w, "Request failed!", http.StatusUnauthorized)
+	}
 
-	/* Create a map to store our claims
-	claims := token.Claims.(jwt.MapClaims)
+	jwtToken := authArr[1]
 
-	/* Set token claims */
-	claims["admin"] = true
-	claims["name"] = "Ado Kukic"
-	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+	claims, err := jwt.ParseWithClaims(jwtToken, &JWTData{}, func(token *jwt.Token) (interface{}, error) {
+		if jwt.SigningMethodHS256 != token.Method {
+			return nil, errors.New("Invalid signing algorithm")
+		}
+		return []byte(SECRET), nil
+	})
 
-	/* Sign the token with our secret */
-	tokenString, _ := token.SignedString(mySigningKey)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Request failed!", http.StatusUnauthorized)
+	}
 
-	/* Finally, write the token to the browser window */
-	w.Write([]byte(tokenString))
-})
+	data := claims.Claims.(*JWTData)
 
-var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
-	ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
-		return mySigningKey, nil
-	},
-	SigningMethod: jwt.SigningMethodHS256,
-})
+	userID := data.CustomClaims["userid"]
+
+	// fetch some data based on the userID and then send that data back to the user in JSON format
+	jsonData, err := getAccountData(userID)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Request failed!", http.StatusUnauthorized)
+	}
+
+	w.Write(jsonData)
+}
+
+func getAccountData(userID string) ([]byte, error) {
+	output := Account{"gstbimo@gmail.com", 3.14, "BTC"}
+	json, err := json.Marshal(output)
+	if err != nil {
+		return nil, err
+	}
+
+	return json, nil
+}
